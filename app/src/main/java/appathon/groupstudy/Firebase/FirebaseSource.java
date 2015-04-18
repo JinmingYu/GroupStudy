b517cb467a45e8eda633b82e211ab26c5ee785f4
Saleh Hamadeh <salehhamadeh@gmail.com>
2015-04-18 02:40:54 -0400

    Remove Child Event Listener.
    Merge remote-tracking branch 'origin/master'
    
    Conflicts:
    	app/app.iml
    	app/src/main/java/appathon/groupstudy/activities/MainActivity.java

--- app/src/main/java/appathon/groupstudy/Firebase/FirebaseSource.java
+++ app/src/main/java/appathon/groupstudy/Firebase/FirebaseSource.java
@@ -1,5 +1,7 @@
 package appathon.groupstudy.Firebase;
 
+import android.util.Log;
+
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -21,7 +23,6 @@ public class FirebaseSource implements IFirebaseSource {
     private Firebase root;
     private Firebase postsRef;
     private Firebase usersRef;
-    private IUpdateActivity updateableActivity;
     private final String FIREBASE_URL = "http://groupstudy.firebaseio.com";
 
     public FirebaseSource(final IUpdateActivity updateableActivity)
@@ -29,44 +30,6 @@ public class FirebaseSource implements IFirebaseSource {
         root = new Firebase(FIREBASE_URL);
         postsRef = root.child("posts");
         usersRef = root.child("users");
-        this.updateableActivity = updateableActivity;
-
-        postsRef.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s)
-            {
-                Post post = dataSnapshot.getValue(Post.class);
-                if(updateableActivity != null)
-                {
-                    FirebaseSource.this.updateableActivity.addPost(post);
-                }
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                Post post = dataSnapshot.getValue(Post.class);
-                if (updateableActivity != null) {
-                    FirebaseSource.this.updateableActivity.updatePost(post);
-                }
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot)
-            {
-                Post post = dataSnapshot.getValue(Post.class);
-                FirebaseSource.this.updateableActivity.removePost(post);
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-
-            }
-        });
     }
 
     @Override
